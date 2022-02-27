46e5925604ed2a094e8b1e6792c28e522c306a60
Bibek Khatri <bibekkhatri291@gmail.com>
2022-02-27 09:51:09 +0545

    Now with Docker support :whale:

--- projectname/settings/dev.py
+++ projectname/settings/dev.py
@@ -2,27 +2,26 @@ import mimetypes
 from .common import *
 
 # Django debug toolbar settings
-
 mimetypes.add_type("application/javascript", ".js", True)
 
 DEBUG = True
 
 SECRET_KEY = "django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "<db_name>",
-        "USER": "<db_username>",
-        "PASSWORD": "<password>",
-        "HOST": "<db_hostname_or_ip>",
-        "PORT": "<db_port>",
-    }
-}
 
 CELERY_BROKER_URL = "redis://redis:6379/0"
 REDIS_URL = "redis://redis:6379/1"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "projectname",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": "5432",
+    }
+}
 
 CACHES = {
     "default": {
@@ -35,7 +34,7 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = "localhost"
+EMAIL_HOST = "mailhog"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 1025
