7b6f8ac3dbb255e002ebc7ea00830d47135ff424
Bibek Khatri <bibekkhatri291@gmail.com>
2023-02-15 23:34:44 +0545

    Add config for prod celery

--- core/celery.py
+++ core/celery.py
@@ -1,13 +1,11 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("core")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
 
 app.autodiscover_tasks()
 
