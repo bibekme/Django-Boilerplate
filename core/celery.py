46a4b6424873b750590c05ed83de943083d276f5
Bibek Khatri <bibekkhatri291@gmail.com>
2023-05-20 22:57:55 +0545

    Move celery setttings to celery file

--- core/celery.py
+++ core/celery.py
@@ -9,6 +9,11 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
 
+
+app.conf.broker_url = os.environ.get(
+    "CELERY_BROKER_URL", "redis://:my_master_password@redis:6379/0"
+)
+
 app.conf.beat_schedule = {
     "periodic_run_task": {
         "task": "common.tasks.run_task",
--- core/settings/base.py
+++ core/settings/base.py
@@ -127,7 +127,3 @@ EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS", False))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "user")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "password")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@example.com")
-
-CELERY_BROKER_URL = os.environ.get(
-    "CELERY_BROKER_URL", "redis://:my_master_password@redis:6379/0"
-)
