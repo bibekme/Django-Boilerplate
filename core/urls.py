73cddcd1786eb2aff69f0619fb5db3d4aea0a803
Bibek Khatri <bibekkhatri291@gmail.com>
2023-05-21 02:28:21 +0545

    Fix static url settings

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
